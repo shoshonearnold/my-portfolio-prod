866c3c69d27e6b23e7eba23fe515ed5010d6fc45
Shoshone Arnold <eastcoastshony@gmail.com>
2020-08-15 11:07:41 -0400

    initialized testing enviorment

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -9,18 +9,20 @@ const Home = props => {
         <div className="homePage">
             <Navbar />
             <main id="home">
-                <h1 className="lg-heading">Shoshone<span className="text-secondary"> Arnold</span></h1>
-                <h2 className="sm-headinghome">Web Developer, Programmer, & Designer</h2>
-                <div className="icons">
-                    <a href="https://www.linkedin.com/in/shoshone-arnold/" target="_blank" rel="noopener noreferrer">
-                        <i className="fab fa-linkedin fa-2x"></i>
-                    </a>
-                    <a href="https://github.com/shoshonearnold" target="_blank" rel="noopener noreferrer">
-                        <i className="fab fa-github fa-2x"></i>
-                    </a>
-                    <a href="https://codepen.io/EastCoastShony/" target="_blank" rel="noopener noreferrer">
-                        <i className="fab fa-codepen fa-2x"></i>
-                    </a>
+                <div className="RWD-home">
+                    <h1 className="lg-heading">Shoshone<span className="text-secondary"> Arnold</span></h1>
+                    <h2 className="sm-headinghome">Web Developer, Programmer, & Designer</h2>
+                    <div className="icons">
+                        <a href="https://www.linkedin.com/in/shoshone-arnold/" target="_blank" rel="noopener noreferrer">
+                            <i className="fab fa-linkedin fa-2x"></i>
+                        </a>
+                        <a href="https://github.com/shoshonearnold" target="_blank" rel="noopener noreferrer">
+                            <i className="fab fa-github fa-2x"></i>
+                        </a>
+                        <a href="https://codepen.io/EastCoastShony/" target="_blank" rel="noopener noreferrer">
+                            <i className="fab fa-codepen fa-2x"></i>
+                        </a>
+                    </div>
                 </div>
             </main>
         </div>
